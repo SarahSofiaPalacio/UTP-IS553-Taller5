d89ef866c8cb796562cf76067e5ee4cc590ce311
SarahSofiaPalacio <s.palacio2@utp.edu.co>
2021-03-11 22:45:55 -0500

    Septimo Ejercicio finalizado sin prueba

---
+++ src/main/java/Ejercicio7/Extremos.java
@@ -0,0 +1,11 @@
+
+package Ejercicio7;
+
+import java.util.List;
+
+
+public interface Extremos<T> {
+    T min(List<T> a);
+    T max(List<T> a);
+
+}
---
+++ src/main/java/Ejercicio7/MyMath.java
@@ -0,0 +1,33 @@
+
+package Ejercicio7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MyMath implements Extremos<Double>{
+
+    protected List<Double> a = new ArrayList<>();
+
+    @Override
+    public Double min(List<Double> a) {
+        double min = a.get(0);
+        for (int i=0; i < a.size(); i++) {
+            if (a.get(i) < min) {
+                min = a.get(i);
+            }
+        } 
+       return min;    }
+
+    @Override
+    public Double max(List<Double> a) {
+        double max =0;
+        for (int i=0; i < a.size(); i++) {
+            if (a.get(i) > max) {
+                max = a.get(i);
+            }
+            ;
+        }
+        return max;    }
+    
+}
