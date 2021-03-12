50eb0ede4d9837c91cd1484ffe7474da69bcf379
SarahSofiaPalacio <s.palacio2@utp.edu.co>
2021-03-11 20:04:58 -0500

    Quinto Ejercicio finalizado sin prueba

--- src/main/java/Ejercicio5/Factura.java
+++ src/main/java/Ejercicio5/Factura.java
@@ -3,8 +3,15 @@ package Ejercicio5;
 
 
 public class Factura extends Precio{
-    
+// Atributos-------------------------------------------------------------------    
     protected String emisor;
     protected String cliente;
-    
+  
+ // Metodos--------------------------------------------------------------------- 
+  
+    public void imprimirFactura(String emisor, String cliente, Double precio) {
+        
+        System.out.println(emisor+" Cobra un total de "
+                           +precio+" al cliente: "+cliente);
+    } 
 }
---
+++ src/test/java/Ejercicio4/PairOfDiceTest.java
@@ -0,0 +1,17 @@
+
+package Ejercicio4;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PairOfDiceTest {
+    
+    public PairOfDiceTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
